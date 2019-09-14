e45774311e9bc643ae90dc9aa6525d851d77db06
Zach  Smith <zdsmith001@gmail.com>
2019-09-14 17:30:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,7 @@ class Student
     @profile_quote = student_hash[:profile_quote]
     @bio = student_hash[:bio]
     @profile_url = student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
@@ -25,6 +26,6 @@ class Student
   end
 
   def self.all
-
+    @@all
   end
 end
